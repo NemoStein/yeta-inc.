31aaeabc2fd094b9f12b3e6b5ead74e0142aca88
NemoStein <contato@nemostein.com.br>
2018-12-08 19:57:53 -0200

    DOM.getTemplate now activates and wraps the template

--- src/js/yetainc/DOM.js
+++ src/js/yetainc/DOM.js
@@ -1,11 +1,18 @@
 export default class Utils
 {
 	/**
-	 * @param {String} reference The classes to look for
+	 * @param {String} key The key of the template
 	 * @param {HTMLElement} [parent] An optional parent to look for the template
+	 * @returns {HTMLElement} An active clone of the template
 	 */
-	static getTemplate(reference, parent)
+	static getTemplate(key, parent)
 	{
-		return (parent || document).querySelector(`template.${reference}`)
+		const template = (parent || document).querySelector(`template.${key}`)
+		const div = document.createElement('div')
+		
+		div.append(document.importNode(template, true))
+		div.classList.add(...key.split('.').map(value => value))
+		
+		return div
 	}
 }
