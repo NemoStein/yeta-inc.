efc780f3b75efae12466a677fafedb3a8e6a4d9f
NemoStein <contato@nemostein.com.br>
2018-12-08 21:16:14 -0200

    Added basic Button

--- src/js/yetainc/components/Button.js
+++ src/js/yetainc/components/Button.js
@@ -1,6 +1,27 @@
 import Display from '../Display.js'
+import DOM from '../DOM.js'
 
 export default class Button extends Display
 {
-	
+	/**
+	 * @param {String} [label]
+	 * @returns {Button}
+	 */
+	static create(label)
+	{
+		const button = /** @type {Button} */ (this.from(DOM.getTemplate('component.button')))
+		button.label = label || ''
+		
+		return button
+	}
+
+	initialize()
+	{
+		this.labelElement = this.querySelector('.label')
+	}
+
+	set label(value)
+	{
+		this.labelElement.textContent = value
+	}
 }
