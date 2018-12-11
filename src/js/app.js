53379d9daa0d806c35a337fee119035df694b5cb
NemoStein <contato@nemostein.com.br>
2018-12-11 14:13:37 -0200

    Disabling contextmenu and touch-action

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
 import YetaInc from './yetainc/YetaInc.js'
 
+document.addEventListener('contextmenu', event =>
+{
+	event.stopImmediatePropagation()
+	event.preventDefault()
+})
+
 document.addEventListener('DOMContentLoaded', () =>
 {
 	const game = /** @type {YetaInc} */ (YetaInc.from(document.getElementById('App')))
