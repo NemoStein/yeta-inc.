443e406a027c8f7c800fd86c0840305741ff4cf8
NemoStein <contato@nemostein.com.br>
2018-12-11 14:19:16 -0200

    Added Button action, enable & disable

--- src/js/yetainc/components/Button.js
+++ src/js/yetainc/components/Button.js
@@ -1,27 +1,70 @@
 import Display from '../Display.js'
 import DOM from '../DOM.js'
 
+/**
+ * @callback OnButtonClick
+ * @param {Button} button The clicked button
+ */
+
 export default class Button extends Display
 {
 	/**
-	 * @param {String} [label]
+	 * @param {String} label
+	 * @param {OnButtonClick} action
 	 * @returns {Button}
 	 */
-	static create(label)
+	static create(label, action)
 	{
 		const button = /** @type {Button} */ (this.from(DOM.getTemplate('component.button')))
-		button.label = label || ''
-		
+
+		button.action = action
+		button.label = label
+
 		return button
 	}
 
 	initialize()
 	{
+		this.enabled = true
 		this.labelElement = this.querySelector('.label')
+		this.addEventListener('click', () =>
+		{
+			if (this.enabled && this.onButtonClick)
+			{
+				this.onButtonClick(this)
+			}
+		})
 	}
 
+	/**
+	 * @param {OnButtonClick} value
+	 */
+	set action(value)
+	{
+		this.onButtonClick = value
+	}
+
+	/**
+	 * @param {String} value
+	 */
 	set label(value)
 	{
 		this.labelElement.textContent = value
+	}
+
+	enable()
+	{
+		this.enabled = true
+		this.classList.remove('disabled')
+		
+		return this
+	}
+
+	disable()
+	{
+		this.enabled = false
+		this.classList.add('disabled')
+		
+		return this
 	}
 }
